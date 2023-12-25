bcd570244947bdeab95694d5bcadee4ef9d077f7
Dave <davidrsitorus04@gmail.com>
2023-12-25 15:08:35 +0700

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.kmtest"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kmtest"
         minSdk = 19
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
