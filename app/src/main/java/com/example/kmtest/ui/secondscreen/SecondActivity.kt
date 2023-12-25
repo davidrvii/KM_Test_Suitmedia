1bbb24b3b3041546ec04a47c609d64fb480ecb30
Dave <davidrsitorus04@gmail.com>
2023-12-25 13:51:17 +0700

    Creating Layout

---
+++ app/src/main/java/com/example/kmtest/ui/secondscreen/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kmtest.ui.secondscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kmtest.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
